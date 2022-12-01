581d2a18284bdeb194a7d3c5dce6188f4b8efdb5
dongmikim <dongmikim>
2022-12-02 00:39:07 +0900

    객체를 민들어서 DOM으로 변환

---
+++ app.js
@@ -0,0 +1,29 @@
+function createDOM(node) {
+  // 문자열일 올 경우 예외처리
+  if (typeof node === 'string') {
+    return document.createTextNode(node)
+  }
+
+  const element = document.createElement(node.tag)
+
+  // 자식요소가 있으면 재귀적으로 호출
+  node.children.map(createDOM).forEach(element.appendChild.bind(element))
+  // context가 깨지기 때문에 bind로 고정
+
+  // 최종적으로 부모를 호출
+  return element
+}
+
+const vdom = {
+  tag: 'p',
+  props: {},
+  children: [
+    {
+      tag: 'span',
+      props: {},
+      children: ['React 만들어 보기'],
+    },
+  ],
+}
+
+document.querySelector('#root').appendChild(createDOM(vdom))
