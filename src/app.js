ec6ddadd449dcb84d0c1ac23c64fe840fe019c1b
dongmikim <dongmikim>
2022-12-02 00:47:10 +0900

    코드 분리

--- src/app.js
+++ src/app.js
@@ -1,18 +1,4 @@
-function createDOM(node) {
-  // 문자열일 올 경우 예외처리
-  if (typeof node === 'string') {
-    return document.createTextNode(node)
-  }
-
-  const element = document.createElement(node.tag)
-
-  // 자식요소가 있으면 재귀적으로 호출
-  node.children.map(createDOM).forEach(element.appendChild.bind(element))
-  // context가 깨지기 때문에 bind로 고정
-
-  // 최종적으로 부모를 호출
-  return element
-}
+import { createDOM } from './react'
 
 const vdom = {
   tag: 'p',
