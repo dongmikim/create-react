fc347f615a9667ef3a4fc58b8843c99e4633f396
dongmikim <dongmikim>
2022-12-02 22:31:12 +0900

    Redux가 동기적으로 동작하는 것 확인

--- src/app.js
+++ src/app.js
@@ -5,10 +5,22 @@ import * as Actions from './actions.js'
 const store = createStore(reducer)
 
 store.subscribe(function () {
-  console.log(store.getState())
+  console.log('subscribe => ', store.getState())
 })
 
 store.dispatch(Actions.increase())
+
+console.log('dispatch => ', store.getState())
+
 store.dispatch(Actions.increase())
+
+console.log('dispatch => ', store.getState())
+
 store.dispatch(Actions.decrease())
+
+console.log('dispatch => ', store.getState())
+
 store.dispatch(Actions.reset())
+
+// 코드 흐름대로 동기적으로 코드가 실행된다
+// 리듀서에서 비동기 작업을 할 수 없다
