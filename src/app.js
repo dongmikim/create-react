646787fa32e5585c67b06f7a3169c5c5c52b7ef4
dongmikim <dongmikim>
2022-12-02 20:37:04 +0900

    state가 undefined일 경우 방어코드 작성

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ function createStore() {
 }
 
 // 상태를 바꾸는 함수
-function worker() {
+function worker(state = {}) {
   // 새로운 상태를 반환해야 한다
   // deep copy해서 참조를 끊어서 새로운 객체를 만들어야 한다는 뜻
   // 우선 depp copy되는 구조는 더 복잡하므로 spread 연산자로 얕은 복사로 진행
