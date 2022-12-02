57deffa37b674f6c70a10a82452149387517e542
dongmikim <dongmikim>
2022-12-02 12:38:30 +0900

    코드 정리 - 안 쓰는 함수 import 제거 - 상수를 함수형 컴포넌트로 수정

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 /* @jsx createElement */
-import { createDOM, createElement, render } from './react'
+import { createElement, render } from './react'
 
 function Title(props) {
   return <h1>{props.children}</h1>
 }
 
-const vdom = (
+const App = () => (
   <div>
     <Title>React 만들기</Title>
-    <p>
-      <span>React 만들기2</span>
-    </p>
   </div>
 )
 
-render(vdom, document.querySelector('#root'))
+render(<App />, document.querySelector('#root'))
